0d63abaf10ef4b6bc25c65e160bc86474b499263
Pete Bevin <pete@petebevin.com>
2012-04-06 14:32:24 -0400

    Problems 5 and 6

--- euler.c
+++ euler.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <memory.h>
+#include <stdlib.h>
+#include <inttypes.h>
 
 void
 eu001(char *ans) {
@@ -106,6 +108,49 @@ eu004(char *ans) {
   sprintf(ans, "%d", largest);
 }
 
+/*
+ * Find the smallest number divisible by all integers
+ * from 1 to 20.
+ *
+ * Each integer in the range contributes a factor.
+ * Primes contribute themselves. Powers of primes
+ * (p^n) contribute the prime (p).  Other composite
+ * numbers contribute nothing.  The answer is the
+ * product of all the factors.
+ *
+ * For n=20, the product fits into 32 bits, but I've
+ * used a 64-bit type to allow n>20.
+ */
+void
+eu005(char *ans) {
+  int n = 20;
+  int i, j;
+  uint64_t product = 1;
+  int *factors = malloc(n * sizeof(int));
+
+  for(i = 2; i < n; i++) {
+    int factor = i;
+    for (j = 2; j < i; j++) {
+      if (factor % factors[j] == 0)
+	factor /= factors[j];
+    }
+    product *= factor;
+    factors[i] = factor;
+  }
+  free(factors);
+
+  sprintf(ans, "%" PRIu64, product);
+}
+
+void eu006(char *ans) {
+  int n = 100;
+  int sum_of_squares = n * (n+1) * (2*n+1) / 6;
+  int sum = n * (n+1) / 2;
+  int square_of_sums = sum * sum;
+
+  sprintf(ans, "%d", square_of_sums - sum_of_squares);
+}
+
 typedef void (solver)(char *ans);
 struct puzzle {
   const char *name;
@@ -117,7 +162,9 @@ struct puzzle puzzles[] = {
   { "001", &eu001, "233168" },
   { "002", &eu002, "4613732" },
   { "003", &eu003, "6857" },
-  { "004", &eu004, "906609" }
+  { "004", &eu004, "906609" },
+  { "005", &eu005, "232792560" },
+  { "006", &eu006, "25164150" },
 };
 
 #define NPUZZLES (sizeof puzzles / sizeof(puzzles[0]))
