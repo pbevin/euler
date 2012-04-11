14203cebec813f0225c4a45a887371b269622f1e
Pete Bevin <pete@petebevin.com>
2012-04-10 21:05:50 -0400

    Problem 15

--- euler.c
+++ euler.c
@@ -411,6 +411,31 @@ void eu014(char *ans) {
   sprintf(ans, "%d", nmax);
 }
 
+/*
+ * The answer in general is (2n)choose(n).
+ * In our case, it's (40! / 20!20!)
+ * We can calculate it using Pascal's Triangle.
+ */
+void eu015(char *ans) {
+  const int N = 20;
+  long long a[N*2+2][N*2+2];
+
+  memset(a, 0, sizeof(a));
+  a[0][0] = 1;
+  a[0][1] = 0;
+  for (int i = 1; i < N*2+2; i++) {
+    a[i][0] = 1;
+    for (int j = 1; j < i; j++) {
+      a[i][j] = a[i-1][j-1] + a[i-1][j];
+    }
+    a[i][i+1] = 0;
+  }
+  sprintf(ans, "%lld", a[41][20]);
+}
+  
+
+  
+
 typedef void (solver)(char *ans);
 struct puzzle {
   const char *name;
@@ -433,6 +458,7 @@ struct puzzle puzzles[] = {
   { "012", &eu012, "76576500" },
   { "013", &eu013, "5537376230" },
   { "014", &eu014, "837799" },
+  { "015", &eu015, "137846528820" },
 };
 
 #define NPUZZLES (sizeof puzzles / sizeof(puzzles[0]))
