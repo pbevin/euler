c3d3cec6c53da5d6c08e58867e462a3f8600f078
Pete Bevin <pete@petebevin.com>
2012-04-15 13:50:57 -0400

    Problem 18

--- euler.c
+++ euler.c
@@ -474,7 +474,59 @@ void eu016(char *ans) {
 void eu017(char *ans) {
   sprintf(ans, "21124");
 }
-  
+
+int max(int x, int y) {
+  if (x > y)
+    return x;
+  else
+    return y;
+}
+
+void eu018(char *ans) {
+  const int N = 15;
+  char data[] = "\
+75 \
+95 64 \
+17 47 82 \
+18 35 87 10 \
+20 04 82 47 65 \
+19 01 23 75 03 34 \
+88 02 77 73 07 63 67 \
+99 65 04 28 06 16 70 92 \
+41 41 26 56 83 40 80 70 33 \
+41 48 72 33 47 32 37 16 94 29 \
+53 71 44 65 25 43 91 52 97 51 14 \
+70 11 33 28 77 73 17 78 39 68 17 57 \
+91 71 52 38 17 14 91 43 58 50 27 29 48 \
+63 66 04 68 89 53 67 30 73 16 69 87 40 31 \
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23";
+  int n = 0;
+  int maxima[N];
+  int row[N];
+
+  memset(maxima, 0, sizeof(maxima));
+  for (int i = 0; i < N; i++) {
+    int newmax[N];
+    memset(newmax, 0, sizeof(newmax));
+    memset(row, 0, sizeof(row));
+    for (int j = 0; j <= i; j++) {
+      row[j] = atoi(data + 3 * n);
+      n++;
+    }
+    newmax[0] = maxima[0] + row[0];
+    for (int j = 0; j < i; j++) {
+      newmax[j + 1] = max(maxima[j], maxima[j+1]) + row[j + 1];
+    }
+    newmax[i] = maxima[i-1] + row[i];
+    memcpy(maxima, newmax, sizeof(maxima));
+  }
+
+  int max = 0;
+  for (int i = 0; i < N; i++) {
+    if (maxima[i] > max) max = maxima[i];
+  }
+  sprintf(ans, "%d", max);
+}
 
 typedef void (solver)(char *ans);
 struct puzzle {
@@ -501,6 +553,7 @@ struct puzzle puzzles[] = {
   { "015", &eu015, "137846528820" },
   { "016", &eu016, "1366" },
   { "017", &eu017, "21124" },
+  { "018", &eu018, "1074" },
 };
 
 #define NPUZZLES (sizeof puzzles / sizeof(puzzles[0]))
