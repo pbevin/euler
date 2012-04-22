22c736d16a511cf0c884590f4fa2de7e3a53c0df
Pete Bevin <pete@petebevin.com>
2012-04-22 12:34:58 -0400

    Problem 34 - sum of digit factorials

--- euler.c
+++ euler.c
@@ -1110,6 +1110,37 @@ void eu033(char *ans) {
   sprintf(ans, "%d", den / g);
 }
 
+/* Find numbers that are the sum of the factorial
+ * of their digits.
+ *
+ * The highest possible number is 9!*7 = 2,540,160.
+ */
+void eu034(char *ans) {
+  int fact[10];
+  int total = 0;
+
+  fact[0] = 1;
+  for (int i = 1, t=1; i < 10; i++) {
+    t *= i;
+    fact[i] = t;
+  }
+
+  for (int i = 10; i <= 2540160; i++) {
+    int sum = 0;
+    int n = i;
+
+    while (n > 0) {
+      int d = n % 10;
+      n /= 10;
+      sum += fact[d];
+    }
+    if (sum == i) {
+       total += sum;
+    }
+  }
+  sprintf(ans, "%d", total);
+}
+
 typedef void (solver)(char *ans);
 struct puzzle {
   const char *name;
@@ -1151,6 +1182,7 @@ struct puzzle puzzles[] = {
   { "031", &eu031, "73682" },
   { "032", &eu032, "45228" },
   { "033", &eu033, "100" },
+  { "034", &eu034, "40730" },
 };
 
 #define NPUZZLES (sizeof puzzles / sizeof(puzzles[0]))
