1d6bb86636db0e60a919b03178ae2763db052204
Pete Bevin <pete@petebevin.com>
2012-04-22 13:50:14 -0400

    Problem 36 - palindromes in bases 10 and 2

--- euler.c
+++ euler.c
@@ -1179,9 +1179,6 @@ int chkcirc(int lo, int hi, int len, char *sieve) {
   }
   return count;
 }
-      
-      
-      
     
 void eu035(char *ans) {
   int sum = 13;
@@ -1195,6 +1192,44 @@ void eu035(char *ans) {
   free(sieve);
 
   sprintf(ans, "%d", sum);
+}
+
+int ispalindrome(char *a) {
+  int i = 0, j = strlen(a) - 1;
+  while (i < j) {
+    if (a[i++] != a[j--])
+      return 0;
+  }
+  return 1;
+}
+
+void base2(int n, char *a) {
+  while (n) {
+    *a++ = (n % 2 == 0) ? '0' : '1';
+    n /= 2;
+  }
+  *a = 0;
+}
+
+/*
+ * Sum of all numbers < 1,000,000 that are palindromic
+ * in base 10 and base 2.
+ */
+void eu036(char *ans) {
+  char buf[30];
+  int t = 0;
+
+  for (int i = 1; i < 1000000; i++) {
+    sprintf(buf, "%d", i);
+    if (ispalindrome(buf)) {
+      base2(i, buf);
+      if (ispalindrome(buf)) {
+	t += i;
+      }
+    }
+  }
+
+  sprintf(ans, "%d", t);
 }
 
 typedef void (solver)(char *ans);
@@ -1240,6 +1275,7 @@ struct puzzle puzzles[] = {
   { "033", &eu033, "100" },
   { "034", &eu034, "40730" },
   { "035", &eu035, "55" },
+  { "036", &eu036, "872187" },
 };
 
 #define NPUZZLES (sizeof puzzles / sizeof(puzzles[0]))
