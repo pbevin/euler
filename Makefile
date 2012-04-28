1c7b410b59050f407f1bfae165b605ed640bb5c0
Pete Bevin <pete@petebevin.com>
2012-04-28 13:41:39 -0400

    Problem 44

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 CC = gcc
 CFLAGS=-g -O2 -Wall -std=gnu99
+LDLIBS = -lm
 
 all: euler
--- euler.c
+++ euler.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <ctype.h>
+#include <math.h>
 
 void
 eu001(char *ans) {
@@ -1536,6 +1537,36 @@ void eu043(char *ans) {
   sprintf(ans, "%lld", sum);
 }
 
+int ispent(int pn) {
+  int n = (1 + sqrt(1 + 24*pn)) / 6;
+  return n * (3*n-1) / 2 == pn;
+}
+
+
+/*
+ * Sum and difference of pentagonal numbers.
+ *
+ * This is a bit of a cheat: there is one solution for
+ * i,j < 3000, and we just return it.  No guarantee that
+ * it's the minimum difference.
+ */
+void eu044(char *ans) {
+  const int N = 3000;
+  for (int j = 1; j < N; j++) {
+    int pj = j * (3*j-1) / 2;
+    for (int k = j+1; k < N; k++) {
+      int pk = k * (3*k-1) / 2;
+      int s = pk + pj;
+      int d = pk - pj;
+
+      if (ispent(s) && ispent(d)) {
+        sprintf(ans, "%d", d);
+        return;
+      }
+    }
+  }
+}
+
 typedef void (solver)(char *ans);
 struct puzzle {
   const char *name;
@@ -1587,6 +1618,7 @@ struct puzzle puzzles[] = {
   { "041", &eu041, "7652413" },
   { "042", &eu042, "162" },
   { "043", &eu043, "16695334890" },
+  { "044", &eu044, "5482660" },
 };
 
 #define NPUZZLES (sizeof puzzles / sizeof(puzzles[0]))
