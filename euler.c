964a37533c85396eddea4483147c41d444d73c12
Pete Bevin <pete@petebevin.com>
2012-05-05 20:47:08 -0400

    Problem 45

--- euler.c
+++ euler.c
@@ -1567,6 +1567,37 @@ void eu044(char *ans) {
   }
 }
 
+/*
+ * Next number after 40755 that is triangular, pentagonal and hexagonal.
+ *
+ * T(n+1) - T(n) = n+1
+ * P(n+1) - P(n) = 3n+1
+ * H(n+1) - H(n) = 4n+1
+ */
+void eu045(char *ans) {
+  int a = 285, b = 165, c = 143;
+  int ta = 40755, pb = ta, hc = ta;
+
+  for (;;) {
+    hc += 4*c+1;
+    c++;
+
+    while (pb < hc) {
+      pb += 3*b+1;
+      b++;
+    }
+    while (ta < hc) {
+      ta += a+1;
+      a++;
+    }
+
+    if (ta == pb && pb == hc) {
+      sprintf(ans, "%d", ta);
+      return;
+    }
+  }
+}
+
 typedef void (solver)(char *ans);
 struct puzzle {
   const char *name;
@@ -1619,6 +1650,7 @@ struct puzzle puzzles[] = {
   { "042", &eu042, "162" },
   { "043", &eu043, "16695334890" },
   { "044", &eu044, "5482660" },
+  { "045", &eu045, "1533776805" },
 };
 
 #define NPUZZLES (sizeof puzzles / sizeof(puzzles[0]))
