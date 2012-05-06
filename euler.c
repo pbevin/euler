34e36b9dadec0d22328ec649c6483e73c6c2881d
Pete Bevin <pete@petebevin.com>
2012-05-05 21:26:20 -0400

    Problem 46

--- euler.c
+++ euler.c
@@ -1598,6 +1598,33 @@ void eu045(char *ans) {
   }
 }
 
+/*
+ * Smallest odd composite that cannot be written as p+2n^2
+ * (prime plus twice a square).
+ */
+void eu046(char *ans) {
+  const int N = 10000;
+  char sieve[N];
+  char hits[N];
+
+  gensieve(sieve, N);
+  memset(hits, 0, N);
+
+  for (int p = 2; p < N; p++) {
+    if (sieve[p]) continue;
+    for (int n = 0; p + 2*n*n < N; n++) {
+      hits[p + 2*n*n] = 1;
+    }
+  }
+
+  for (int i = 9; i < N; i += 2) {
+    if (sieve[i] && hits[i] == 0) {
+      sprintf(ans, "%d", i);
+      return;
+    }
+  }
+}
+
 typedef void (solver)(char *ans);
 struct puzzle {
   const char *name;
@@ -1651,6 +1678,7 @@ struct puzzle puzzles[] = {
   { "043", &eu043, "16695334890" },
   { "044", &eu044, "5482660" },
   { "045", &eu045, "1533776805" },
+  { "046", &eu046, "5777" },
 };
 
 #define NPUZZLES (sizeof puzzles / sizeof(puzzles[0]))
