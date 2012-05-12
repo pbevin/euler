cb4ff587f440c5afd39d1faf44bfa939e13a202d
Pete Bevin <pete@petebevin.com>
2012-05-12 16:17:26 -0400

    Problem 50

--- euler.c
+++ euler.c
@@ -1730,6 +1730,48 @@ void eu049(char *ans) {
   }
 }
 
+/*
+ * Find the longest sequence of primes under 1,000,000
+ * whose sum is a prime.
+ */
+void eu050(char *ans) {
+  const int N = 1000000;
+
+  int *primes = malloc(N/2 * sizeof(int));
+  int n = genprimes(primes, N);
+
+  char *sieve = malloc(N);
+  gensieve(sieve, N);
+
+  int *sums = (int *)malloc(n * sizeof(int));
+  memset(sums, 0, n * sizeof(int));
+  int t = 0;
+
+  for (int i = 0; i < n; i++) {
+    t += primes[i];
+    sums[i] = t;
+  }
+
+  int maxlen = 0;
+  int maxsum = 0;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = i; sums[j]-sums[i] < N; j++) {
+      int sum = sums[j] - sums[i];
+      if (j - i > maxlen && sieve[sum] == 0) {
+        maxsum = sum;
+        maxlen = j - i;
+      }
+    }
+  }
+  sprintf(ans, "%d", maxsum);
+
+  free(primes);
+  free(sieve);
+}
+
+
+
 typedef void (solver)(char *ans);
 struct puzzle {
   const char *name;
@@ -1787,6 +1829,7 @@ struct puzzle puzzles[] = {
   { "047", &eu047, "134043" },
   { "048", &eu048, "9110846700" },
   { "049", &eu049, "296962999629" },
+  { "050", &eu050, "997651" },
 };
 
 #define NPUZZLES (sizeof puzzles / sizeof(puzzles[0]))
