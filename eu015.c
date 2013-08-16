f06d0dec05d404b2a51208b0967708e62d6bdea8
Pete Bevin <pete@hover.com>
2013-08-16 09:39:46 -0400

    Fix eu015 memory access that causes a crash

--- eu015.c
+++ eu015.c
@@ -7,17 +7,19 @@
  */
 void eu015(char *ans) {
   const int N = 20;
-  long long a[N*2+2][N*2+2];
+  const int HEIGHT = N*2+1;
+  const int WIDTH = N*2+1;
+  long long a[HEIGHT][WIDTH];
 
   memset(a, 0, sizeof(a));
+
   a[0][0] = 1;
-  a[0][1] = 0;
-  for (int i = 1; i < N*2+2; i++) {
+  for (int i = 1; i < HEIGHT; i++) {
     a[i][0] = 1;
     for (int j = 1; j < i; j++) {
       a[i][j] = a[i-1][j-1] + a[i-1][j];
     }
-    a[i][i+1] = 0;
+    a[i][i] = 1;
   }
-  sprintf(ans, "%lld", a[41][20]);
+  sprintf(ans, "%lld", a[40][20]);
 }
