a3c1edfe585aedd990f4233ba1168ba2d2822569
Pete Bevin <pete@petebevin.com>
2013-12-31 10:34:06 -0500

    Improved algorithm for problem 76

--- eu076.c
+++ eu076.c
@@ -3,41 +3,38 @@
 #define N 100
 
 
+//
+// Using Euler's generating function for a recurrence relation:
+// see equation (11) on this page:
+//   http://mathworld.wolfram.com/PartitionFunctionP.html
+//
+// We cache values in the P array so that P[n] is the partition
+// number of n.  The question asks for the ways 2 or more numbers
+// can sum to the value, so P() is off by one because it includes
+// the degenerate sum n = n.
+//
 void eu076(char *ans) {
-  int sum[N+1][N+1];
-  memset(sum, 0, sizeof(sum));
+  static int P[N+1];
+  memset(P, 0, sizeof(P));
 
-  // sum[i][j] is the number of ways of making i with numbers
-  // less than or equal to j.
-  // 
-  // sum[i][1] = i
-  // sum[N][N] is the answer.
+  P[0] = 1;
+  P[1] = 1;
 
-  for (int i = 1; i <= N; i++) {
-    sum[i][1] = 1;
-    for (int j = 2; j < i; j++) {
-      int s;
-      // We can make i from j + (i-j) as long as i-j
-      // is made up of numbers less than j
-      // There are sum[i-j][j] ways to do this.
-      // The extra 1 comes from using j and i-j directly
-      // as a two-sum. This is only possible if i-j is no
-      // more than j.
-      
-      if (i - j <= j) {
-        s = 1;
-      } else {
-        s = 0;
-      }
-      sum[i][j] = sum[i][j-1] + sum[i-j][j] + s;
+  for (int n = 2; n <= N; n++) {
+    int sign = 1;
+    int sum = 0;
+    for (int k = 1; ; k++, sign = -sign) {
+      int n1 = n - k * (3*k-1) / 2;
+      int n2 = n - k * (3*k+1) / 2;
+
+      if (n1 < 0) break;
+
+      sum += P[n1] * sign;
+      if (n2 >= 0) sum += P[n2] * sign;
     }
 
-    for (int j = i; j <= N; j++) {
-      sum[i][j] = sum[i][i-1];
-    }
-
-    sum[i][i] = sum[i][i-1];
+    P[n] = sum;
   }
 
-  sprintf(ans, "%d", sum[N][N]);
+  sprintf(ans, "%d", P[N] - 1);
 }
