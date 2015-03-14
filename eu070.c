317d2158b400a221fa3b2c863f74e86840611ae4
Pete Bevin <pete@petebevin.com>
2015-03-14 08:07:26 -0400

    Better algorithm for 070
    Speeds up from 0.85s to 0.03

--- eu070.c
+++ eu070.c
@@ -2,24 +2,46 @@
 
 #define N 10000000
 
+/*
+ * To minimize the ratio, we would like the answer to be a prime,
+ * since this maximizes phi(n) = n-1.  But n-1 can't be a permutation
+ * of n (only the last digit will change if n is prime)
+ *
+ * Next best guess is the product of a pair of primes close to sqrt(N)
+ * since then phi(p)*phi(q) = (p-1)(q-1).
+ *
+ * So we'll search pairs of primes up to 2*sqrt(N) and see where that
+ * gets us.
+ */
 void
 eu070(char *ans) {
-  static int phi[N];
-
-  genphi(phi, N);
+  int maxprime = 2 * sqrt(N);
+  int *primes = malloc(maxprime * sizeof(int));
+  int nprimes = genprimes(primes, maxprime);
 
   double min = 1000.0;
-  int i_of_min = 0;
-  for (int i = 2; i < N; i++) {
-    if (isperm(i, phi[i])) {
-      double ratio = (double)i / phi[i];
-      if (ratio < min) {
-        /* printf("%d %d %g\n", i, phi[i], ratio); */
-        min = ratio;
-        i_of_min = i;
+  int n_of_min = 0;
+
+  for (int i = 0; i < nprimes; i++) {
+    int p = primes[i];
+    for (int j = 0; j < nprimes; j++) {
+      int q = primes[j];
+      int n = p * q;
+      if (n > N) break;
+      int phi = (p-1) * (q-1);
+
+      if (isperm(n, phi)) {
+        double ratio = (double)n / phi;
+
+        if (ratio < min) {
+          /* printf("%d*%d=%d %d %g\n", p, q, n, phi, ratio); */
+          min = ratio;
+          n_of_min = n;
+        }
       }
     }
   }
+  free(primes);
 
-  sprintf(ans, "%d", i_of_min);
+  sprintf(ans, "%d", n_of_min);
 }
